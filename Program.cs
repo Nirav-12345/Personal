27e0ece572a17535c1860bd8edcbefd1cc2ec57b
Nirav-12345 <98863721+Nirav-12345@users.noreply.github.com>
2022-03-28 21:05:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HarmonicTerm;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        double h = 1;
+
+        for (float i=2;i<=n;i++)
+        {
+            h = h + 1 / i;
+
+            Console.WriteLine(h);
+        }
+    }
+}
